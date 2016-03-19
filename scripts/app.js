467242caf791380e42ed8ae35b0f838eeadfd920
Mohamed Hassan <mohamedhdesign@gmail.com>
2016-03-19 14:21:23 -0700

    Second main commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,6 @@
 // DO NOT WRAP (function(){}); AROUND JAVASCRIPT CODE
 $(document).ready(function(){
   // JQuery goodness
-  $('input').on('click', function(){
-    alert('connected!!');
-  });
   window.onload = function() {
     setTimeout( function(){
       $('#loader').fadeOut();
