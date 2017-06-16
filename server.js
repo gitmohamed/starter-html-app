7fb2299561620c4a76ec7ee4fe054356bc1b3c6a
Mohamed Hassan <mohamedhdesign@gmail.com>
2017-06-15 21:35:14 -0700

    Made port variable for production use

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const ejs = require('ejs')
 const app = express()
 
+const port = process.env.port || 3003
+
 app.set('view engine', 'ejs')
 app.set('views', 'views/')
 app.use(express.static('dist'))
@@ -10,6 +12,6 @@ app.get('/', (req, res) => {
   res.render('index', {title: 'Mohamed Hassan'})
 })
 
-app.listen(3003, () => {
-  console.log('Example app listening on port 3003!')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
 })
